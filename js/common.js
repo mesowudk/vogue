62489d3d8d8eb6d97ec5775cba53a6cd25018117
mesowudk <52552020+mesowudk@users.noreply.github.com>
2019-07-09 10:17:30 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,1 +1,50 @@
 
+ 
+     $(function(){
+         $("#modal").hide();
+         $("#login,#login_icon").on("click",function(){
+             $("#modal").show();
+             $("#close_btn").on("click",function(){
+              $("#modal").hide();
+            });
+         });
+                   
+        $("#search_form").hide();
+        $(".search_icon").on("click",function(){
+            $("#search_form").slideToggle();
+            if(window.innerWidth<=960){
+                $("nav").hide();
+            }
+        });
+    
+        $(window).on("resize",function(){
+            if(window.innerWidth>960){
+                $("nav").show();
+            }
+        });
+    
+        $(window).on("resize",function(){
+            if(window.innerWidth<=960){
+            $("nav").hide();
+            }
+
+        });
+
+        $("#tgl_menu").on("click",function(){
+            $("nav").slideToggle();
+            $("#search_form").hide();
+        });
+
+      
+        $("#side_menu").on("mouseover",function(){
+            $("#side_menu_box").css("visibility","visible");
+        });
+
+        $("#side_menu_box").on("mouseout",function(){
+            $(this).css("visibility","hidden");
+        });
+    
+   
+    });
+
+
